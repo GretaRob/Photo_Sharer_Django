4f8e7b94d2e68a30ad1bc44ce3c199709a7c5513
GretaRob <greta.robichaud@gmail.com>
2021-02-03 18:06:46 +0700

    edited

--- Photo_Sharer/models.py
+++ Photo_Sharer/models.py
@@ -6,3 +6,7 @@ from django.db import models
 class Image (models.Model):
     photo = models.ImageField(upload_to='images')
     date = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
